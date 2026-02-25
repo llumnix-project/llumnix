d9e4442d92ecd775e1eec305422e2e774818bc50
jiangjiemin.jjm <jiangjiemin.jjm@antgroup.com>
2026-02-25 17:33:26 +0800

    fix: fix blade-kvt install && build llumlet proto before tests Link: https://code.alibaba-inc.com/cuikuilong.ckl/llumnix/codereview/26058182 * fix: fix blade-kvt install && build llumlet proto before tests

--- scripts/install_barex.sh
+++ scripts/install_barex.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
+apt remove -y accl-barex-cuda || true
 wget https://eflop.oss-cn-beijing.aliyuncs.com/accl-barex/tcp/accl-barex-pkg-test-tcp-v1.1.3/accl-barex-cuda12.2-devel-1.5.2-2-tcp.deb
 dpkg -i accl-barex-cuda12.2-devel-1.5.2-2-tcp.deb
